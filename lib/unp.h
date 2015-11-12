9cda0f7410c66eec52cf7e8576ba280e600d95a7
zhangxiuyun <zhangxiuyun@shinezone.com>
2015-11-12 15:19:39 +0800

    daytimetcpcli

---
+++ lib/unp.h
@@ -0,0 +1,13 @@
+#ifndef __UNP_H__
+#define __UNP_H__
+
+#include <netinet/in.h> // for sockaddr_in{} 
+#include <sys/socket.h> // socket() connect() bind() listen()
+						// read() write() etc.
+
+
+#define MAXLINE  100
+
+
+
+#endif
